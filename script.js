b383dac93c4f5518683a305671b55d1410a5ed0a
pacalica <policagabrielvictor@gmail.com>
2025-07-12 10:56:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let web3;
 let contract;
-const contractAddress = "0x124377FCe14439248a4959ce528314aA3A897321"; // <- înlocuiește cu adresa ta reală
+const contractAddress = "0x124377FCe14439248a4959ce528314aA3A897321"; //
 
 async function connectWallet() {
   if (window.ethereum) {
